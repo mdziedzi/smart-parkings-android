961226ddb20f2fda6061b35971287c22971d8ca8
mdziedzi <marcindziedzic8@gmail.com>
2018-11-17 17:53:44 +0100

    Add first UI test.

---
+++ app/src/androidTest/java/com/marcindziedzic/smartparkingsandroid/Login/LoginActivityTest.java
@@ -0,0 +1,39 @@
+package com.marcindziedzic.smartparkingsandroid.Login;
+
+
+import com.marcindziedzic.smartparkingsandroid.MainActivity;
+import com.marcindziedzic.smartparkingsandroid.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void typeSomething() {
+        String s = "this is test";
+
+        onView(withId(R.id.agentNameText)).perform(typeText(s), closeSoftKeyboard());
+        onView(withId(R.id.agentNameText)).check(matches(withText(s)));
+    }
+}
